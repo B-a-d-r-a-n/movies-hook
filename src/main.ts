8960bf0f7bdf3e885209287755a5f1c5793d98da
Mohamed Badran <mohamed.ahmed.badran0@gmail.com>
2025-08-22 00:21:24 +0300

    feat(env): setting up initial environment

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import './main.css'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+// import Aura from '@primeuix/themes/aura'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue, {
+  unstyled: true,
+})
 app.use(router)
 
 app.mount('#app')
