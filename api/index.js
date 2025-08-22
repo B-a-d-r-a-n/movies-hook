a30d61367ac4a8231ae62567549f3d59a1213875
Mohamed Badran <mohamed.ahmed.badran0@gmail.com>
2025-08-22 05:34:09 +0300

    refactor(api): restructure server export and improve CORS handling

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
 const jsonServer = require('json-server')
-const path = require('path')
-
-const dbPath = path.join(__dirname, 'db.json')
-
 const server = jsonServer.create()
-const router = jsonServer.router(dbPath)
+const router = jsonServer.router(require('path').join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
 
-module.exports = server
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
+  server(req, res)
+}
